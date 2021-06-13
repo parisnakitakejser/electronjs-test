60e8af1e6d09f07686df43c7bf96629c47692620
Paris Nakita Kejser <contact@parisnakitakejser.com>
2021-06-13 20:09:11 +0200

    v1.0.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# Eletronjs - Test application
+- GitHub action builder: https://github.com/marketplace/actions/electron-builder-action
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import { autoUpdater } from "electron-updater"
+const { NsisUpdater } = require('electron-updater')
 const { app, BrowserWindow } = require('electron')
+
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -11,5 +12,20 @@ function createWindow () {
 }
 
 app.whenReady().then(() => {
+    const options = {
+        requestHeaders: {
+            // Any request headers to include here
+            Authorization: 'Basic AUTH_CREDS_VALUE'
+        },
+        provider: 'github',
+        url: 'https://example.com/auto-updates'
+    }
+    
+    if (process.platform === "win32") {
+        autoUpdater = new NsisUpdater(options)
+    }
+
+    autoUpdater.checkForUpdatesAndNotify()
+
   createWindow()
 })
